ce0e8c3cd8302175fd18a80f79f6a0782e27bc8e
Electro593 <electro5939@gmail.com>
2022-04-20 21:15:53 -0400

    Some svg icons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,48 @@
 var rhit = rhit || {};
 
-rhit.main = function() {
+rhit.Display = class {
+   constructor() {
+      this.favorited = false;
+   }
    
+   setFavorited(newValue) {
+      this.favorited = newValue;
+   }
+};
+
+rhit.init = function() {
+   rhit.menuIsVisible = false;
+   rhit.display = new rhit.Display();
+   
+   let menuIcon = document.querySelector("#menuIcon");
+   menuIcon.onclick = (event) => {
+      let menuPage = document.querySelector("#menuPage");
+      //TODO: Animation
+      if(rhit.menuIsVisible) {
+         menuPage.style.display = "none";
+         menuIcon.src = "images/menu_white.svg";
+      } else {
+         menuPage.style.display = "flex";
+         menuIcon.src = "images/menu_red.svg";
+      }
+      rhit.menuIsVisible = !rhit.menuIsVisible;
+   };
+   
+   let favoriteStar = document.querySelector("#favoriteStar");
+   favoriteStar.onclick = (event) => {
+      if(rhit.display.favorited) {
+         rhit.display.setFavorited(false);
+         favoriteStar.src = "images/star_gray.svg";
+      } else {
+         rhit.display.setFavorited(true);
+         favoriteStar.src = "images/star_gold.svg";
+      }
+   }
+};
+
+
+rhit.main = function() {
+   rhit.init();
 };
 
 rhit.main();
